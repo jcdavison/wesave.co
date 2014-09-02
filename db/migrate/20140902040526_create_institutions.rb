00ee3057130279e1cc61215c2b20e20ee873a77e
John Davison <johncdavison@gmail.com>
2014-09-01 21:07:49 -0700

    User has n Institutions.

---
+++ db/migrate/20140902040526_create_institutions.rb
@@ -0,0 +1,11 @@
+class CreateInstitutions < ActiveRecord::Migration
+  def change
+    create_table :institutions do |t|
+      t.string :name
+      t.string :token
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140825161853) do
+ActiveRecord::Schema.define(version: 20140902040526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "institutions", force: true do |t|
+    t.string   "name"
+    t.string   "token"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false

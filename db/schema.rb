2993a875ba5f5e4088999c3593beb25de8ed7b1c
John Davison <johncdavison@gmail.com>
2014-11-03 22:53:42 -0800

    Remove tables.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141103221431) do
+ActiveRecord::Schema.define(version: 20141104065216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,23 +40,6 @@ ActiveRecord::Schema.define(version: 20141103221431) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "item_id"
-  end
-
-  create_table "budget_events", force: true do |t|
-    t.string   "description"
-    t.float    "value"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "month_day",   default: 1
-  end
-
-  create_table "financial_summaries", force: true do |t|
-    t.float    "value"
-    t.string   "user_id"
-    t.string   "integer"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "institutions", force: true do |t|

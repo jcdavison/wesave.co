9c7be395a6ed7c5a5df4bc1271fe751109f0d246
John Davison <johncdavison@gmail.com>
2014-12-03 14:47:38 -0800

    Debug specs.

--- spec/support/plaid_context.rb
+++ spec/support/plaid_context.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.shared_context "plaid sandbox authorization", :a => :b do
   before do
     @user = FactoryGirl.create :user
-    @params = {institution: { username: 'plaid_test', password: 'plaid_good', type: 'Wells Fargo'}}
+    @params = {institution: { username: 'plaid_test', password: 'plaid_good', type: 'Wells Fargo', pin: ''}}
     response = Plaid.new().initiate_auth @params, @user.email
     @api_data = JSON.parse(response.body)
   end
